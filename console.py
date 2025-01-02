072b67190b4407356a5118aff97bcedc9ecc9318
lensonlovescode <lensonisinparis@gmail.com>
2025-01-02 08:17:11 +0300

    bug free

--- console.py
+++ console.py
@@ -136,7 +136,6 @@ class HBNBCommand(cmd.Cmd):
                 value = keyvalue[1].replace("\"", "").replace("_", " ")
                 print(value)
                 setattr(obj, key, value)
-            
         obj.save()
         print(f"{obj.id}")
 
@@ -333,6 +332,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
