8c212034737136e42e0c5402c85a10ce506731db
nepodude <nepo.jean73@gmail.com>
2025-01-06 00:10:45 +0200

    update

--- models/city.py
+++ models/city.py
@@ -11,5 +11,5 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan', backref="cities")
