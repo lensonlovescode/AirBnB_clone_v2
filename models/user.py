6b312a16614d8daa151b75e1658e3ca9f84fc9d0
lensonlovescode <lensonisinparis@gmail.com>
2025-01-05 14:18:38 +0300

    users tsble

--- models/user.py
+++ models/user.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""
+This module defines a class User
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from os import getenv
 
+class User(BaseModel, base):
+    """
+    This class defines a user by various attributes
+    """
+    __tablename__ = users
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if getenv(HBNB_TYPE_STORAGE, default="fs") == "db":
+        email = column(String(128), nullable=False)
+        password = column(String(128), nullable=False)
+        first_name = column(String(128), nullable=True)
+        last_name = column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
