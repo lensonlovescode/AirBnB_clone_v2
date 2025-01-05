d5647bc42c2c39952147afd204c3fd087c0e8247
nepodude <nepo.jean73@gmail.com>
2025-01-05 20:20:50 +0200

    updated the file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """DBStorage engine"""
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 class DBStorage:
     """Database storage engine"""
