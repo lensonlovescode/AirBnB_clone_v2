c50a77144973475adbe96c4bcac0bcafdccd7705
lensonlovescode <lensonisinparis@gmail.com>
2025-02-07 15:03:57 +0300

    <int:n>

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Contains a script that starts a Flask web application 0.0.0.0, port 5000
+"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_bnb():
+    """
+    Displays “Hello HBNB!” on route /
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+    Displays HBNB on route /hbnb
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    Displays “C ” followed by the value of the text variable
+    """
+    text = text.replace("_", " ")
+    return f"C {escape(text)}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """
+    Displays “Python ”, followed by the value of the text variable
+    """
+    text = text.replace("_", " ")
+    return f"Python {escape(text)}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
