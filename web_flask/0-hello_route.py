da92e7e048cdb9d5625d5ce9764bd06064582f14
lensonlovescode <lensonisinparis@gmail.com>
2025-02-07 13:45:57 +0300

    Hello Route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Contains a script that starts a Flask web application 0.0.0.0, port 5000
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_bnb():
+    """
+    Displays “Hello HBNB!” on route /
+    """
+    return "Hello HBNB!"
